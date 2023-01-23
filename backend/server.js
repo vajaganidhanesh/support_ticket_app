32ce093590b72e9c509fdf7652e50e5ad17dbec2
vajagani dhanesh <vajaganidhanesh@gmail.com>
2023-01-23 15:31:17 +0530

    middle ware for error handler is added

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,15 @@
 const express =  require('express')
 const dotenv = require('dotenv').config()
+const {errorHandler} = require('./middleware/errorMiddleware')
 const PORT = process.env.PORT || 8000
 
 const app = express()
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
 
+//Routes
 app.use('/users',require('./routes/UserRoute'))
+app.use(errorHandler)
 
 app.listen(PORT,()=>{
     console.log(`Server started on port ${PORT}`);
