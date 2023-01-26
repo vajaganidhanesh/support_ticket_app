53245fa63abc716810a3213abc2cdd6122de6ab4
vajagani dhanesh <vajaganidhanesh@gmail.com>
2023-01-26 13:45:04 +0530

    redux part done

---
+++ frontend/src/features/auth/authSlice.js
@@ -0,0 +1,27 @@
+import { createAsyncThunk, createSlice, } from "@reduxjs/toolkit";
+
+const initialState = {
+    user: null,
+    isError: false,
+    isSuccess: false,
+    isLoading: false,
+    message: ''
+}
+
+export const register = createAsyncThunk(
+    'auth/register',
+    async(user,thunkAPI) =>{
+        console.log(user);
+    }
+)
+
+export const authSlice = createSlice({
+    name:'auth',
+    initialState,
+    reducers:{},
+    extraReducers: (builder) =>{
+        
+    }
+})
+
+export default authSlice.reducer
