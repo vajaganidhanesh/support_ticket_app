264b39d147d810a4c108d92366ee90c1de88a013
vajagani dhanesh <vajaganidhanesh@gmail.com>
2023-01-29 20:53:32 +0530

    adding note controller and route has been done

--- backend/controllers/noteController.js
+++ backend/controllers/noteController.js
@@ -1,8 +1,35 @@
 const asyncHandler = require("express-async-handler");
 
 const User = require("../models/userModel");
-const Note = require("../models/noteModel")
+const Note = require("../models/noteModel");
 const Ticket = require("../models/ticketModel");
+
+// @desc   Create ticket note
+// @route  POST /api/tickets/:ticketId/notes
+// @access Private
+const addNote = asyncHandler(async (req, res) => {
+  // Get user using the id in the JWT
+  const user = await User.findById(req.user.id);
+
+  if (!user) {
+    res.status(401);
+    throw new Error("User not found");
+  }
+
+  const ticket = await Ticket.findById(req.params.id);
+
+  if (ticket.user.toString() !== req.user.id) {
+    res.status(401);
+    throw new Error("User not authorized");
+  }
+  const note = await Note.create({
+    text: req.body.text,
+    isStaff: false,
+    ticket: req.params.ticketId,
+    user: req.user.id,
+  });
+  res.status(200).json(note);
+});
 
 // @desc   Get notes for a tickets
 // @route  GET /api/tickets/:ticketId/notes
@@ -16,16 +43,17 @@ const getNotes = asyncHandler(async (req, res) => {
     throw new Error("User not found");
   }
 
-  const ticket = await Ticket.findById(  req.params.id );
+  const ticket = await Ticket.findById(req.params.id);
 
-  if(ticket.user.toString() !== req.user.id){
-    res.status(401)
-    throw new Error('User not authorized')
+  if (ticket.user.toString() !== req.user.id) {
+    res.status(401);
+    throw new Error("User not authorized");
   }
-  const notes = await Note.find({ticket:req.params.ticketId})
+  const notes = await Note.find({ ticket: req.params.ticketId });
   res.status(200).json(notes);
 });
 
 module.exports = {
-    getNotes,
-}
+  getNotes,
+  addNote
+};
